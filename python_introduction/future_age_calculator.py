300c5fa48de4991947d6dbe9f59fae0cd76b48f4
Luke Oladejo <bylukeo@gmail.com>
2025-05-25 10:04:10 +0100

    corrected user input question

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,7 +1,7 @@
 # Python script that asks the user for their current age and then calculates how old they will be in a specific future year, e.g in 50 years. 
 # Using formula: Future Age = Current Age + (Future Year - Current Year)
 
-current_age = int(input("Enter your current age: "))
+current_age = int(input("How old are you?: "))
 future_year = 2050  # future year we are calculating for
 current_year = 2023
 future_age = current_age + (future_year - current_year)
