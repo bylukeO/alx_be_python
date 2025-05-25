5e64e840972dd3074154c5de0ea577b8b1cca3b2
Luke Oladejo <bylukeo@gmail.com>
2025-05-25 09:39:31 +0100

    added comments to code

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,8 @@
+# python code to calculate simple interest
+# Using formula: Simple Interest = Principal * Rate * Time
+
 principal = 1000
 rate = 0.05
 time = 3
-interest = (principal * rate * time)
+interest = principal * rate * time
 print( "The", "simple interest", "is", interest)
