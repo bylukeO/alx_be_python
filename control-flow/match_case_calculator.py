5c0c6ec6260f51b1f6b146552593864c0f98b568
Luke Oladejo <xiiiz.444@gmail.com>
2025-06-02 23:01:21 +0100

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -10,19 +10,21 @@ def multiply_numbers(a, b):
 def divide_numbers(a, b):
     return a / b if b != 0 else "Cannot divide by zero"
 
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 operation = input("Choose the operation (+, -, *, /): ")
 
-if operation == "+":
-    result = add_numbers(num1, num2)
-elif operation == "-":
-    result = subtract_numbers(num1, num2)
-elif operation == "*":
-    result = multiply_numbers(num1, num2)
-elif operation == "/":
-    result = divide_numbers(num1, num2)
-else:
-    result = "Invalid operation"
+match operation:
+    case "+":
+        result = add_numbers(num1, num2)
+    case "-":
+        result = subtract_numbers(num1, num2)
+    case "*":
+        result = multiply_numbers(num1, num2)
+    case "/":
+        result = divide_numbers(num1, num2)
+    case _:
+        result = "Invalid operation"
+
 print("The result is:", result)
