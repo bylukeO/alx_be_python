6191a3d420b1f654b578292150ff4a9c23d1a5ad
Luke Oladejo <xiiiz.444@gmail.com>
2025-06-02 23:13:03 +0100

    Update multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,5 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-number = int(input("Enter a number to generate its multiplication table: "))
+number = int(input("Enter a number to see its multiplication table: "))
 for i in numbers:
     result = number * i
     print(f"{number} x {i} = {result}")
